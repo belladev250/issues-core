2f5371792b6e1935e2520af58958ce7afcff357e
belladev250 <lissadev17@gmail.com>
2025-01-13 16:27:17 +0200

    date fetching

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
+interface User{
+    id:number,
+    name:string
+}
 
-const UsersPage = () => {
+const UsersPage = async () => {
+
+    const res = await fetch('https://jsonplaceholder.typicode.com/users')
+    const users:User[] = await res.json();
   return (
-    <div>
-      Users Page
-    </div>
+   <>
+   <h1>Users List</h1>
+   <ul>
+    {users.map(user=> <li key={user.id}>{user.name}</li>)}
+   </ul>
+   </>
   )
 }
 
